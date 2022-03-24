5d88648ed4b188ee5fb56658f57b057010e43ce1
Luiz Gustavo Paredes Delgado <92489138+gustavoPdelgado@users.noreply.github.com>
2022-03-23 21:05:46 -0400

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,10 @@
-/*
-Quando clicar no icone do pokémon na listagem, temos que esconder o cartão do pokémon aberto e mostrar o cartão correspondente ao que foi selecionado na listagem.
-
-Para isso vamos precisar trabalhar com 2 elementos:
-1 - listagem 
-2 - cartão pokémon
-
-precisamos criar duas variáveis no JS pra trabalhar com os elementos na tela.
-
-Vamos precisar trabaçhar com um evento de clique do usuário na listagem de pokémons
-
--Remover a classe aberto do cartão que está aberto 
--Ao clicar em um pokémon da lsitagem pegamos o id desse pokémon pra saber qual cartão mostrar.
--Remover a classe ativa que marca o pokémon selecionado
--Adicionar a classe ativo no item da lista selecionado
-*/
-
 const listaSelecaoPekemons = document.querySelectorAll('.pokemon')
 const pokemonsCard = document.querySelectorAll('.cartao-pokemon') 
 
 listaSelecaoPekemons.forEach(pokemon => {
     pokemon.addEventListener('click', () => {
-        //remover a classe só do pokemon que está aberto
         const  cartaoPokemonAberto = window.document.querySelector('.aberto')
         cartaoPokemonAberto.classList.remove('aberto')
-        //Ao clicar em um pokémon da listagem pegamos o id desse pokémon pra saber qual cartão mostrar
         const idPokemonSelecionado = pokemon.attributes.id.value
 
         const cartaoPokemonParaAbrir = window.document.getElementById('cartao-'+idPokemonSelecionado)
